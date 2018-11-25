178071ba4761b17067ffa2bf17a8c71abee0471d
Alexander Changli <changli.alexander@gmail.com>
2018-11-25 22:50:38 +0200

    WIP API app deployment config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  // TODO: take from config
   await app.listen(4000);
 }
 bootstrap();
