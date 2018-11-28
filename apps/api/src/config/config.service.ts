0ffefd6324f73c2628e6e69004acaef1c4559b03
Alexander Changli <changli.alexander@gmail.com>
2018-11-28 23:49:31 +0200

    Mongo dev flow + db config

--- apps/api/src/config/config.service.ts
+++ apps/api/src/config/config.service.ts
@@ -16,15 +16,38 @@ export class ConfigService {
    * Also validates values and throws error in case of a value is missing or invalid.
    */
   private getConfigFromEnvVars(): EnvConfig {
-    if (!process.env.API_SERVING_PORT) {
-      throw new Error('API_SERVING_PORT env var is not defined');
-    }
-    const API_SERVING_PORT = Number(process.env.API_SERVING_PORT);
-    if (isNaN(API_SERVING_PORT)) {
-      throw new Error('API_SERVING_PORT env var has invalid value');
+    return {
+      API_SERVING_PORT: this.getAndValidateNumericEnvParam('API_SERVING_PORT'),
+      MONGO_HOST: this.getAndValidateStringEnvParam('MONGO_HOST'),
+      MONGO_PORT: this.getAndValidateNumericEnvParam('MONGO_PORT'),
+      MONGO_USER: this.getAndValidateStringEnvParam('MONGO_USER'),
+      MONGO_PASSWORD: this.getAndValidateStringEnvParam('MONGO_PASSWORD'),
+      MONGO_DB_NAME: this.getAndValidateStringEnvParam('MONGO_DB_NAME'),
+    };
+  }
+
+  private getAndValidateNumericEnvParam(envConfigKey: keyof EnvConfig): number {
+    const numericValue = Number(
+      this.getAndValidateStringEnvParam(envConfigKey),
+    );
+
+    if (isNaN(numericValue)) {
+      throw new Error(
+        `${envConfigKey} environment configuration variable is has invalid value ${numericValue}`,
+      );
     }
 
-    return { API_SERVING_PORT };
+    return numericValue;
+  }
+
+  private getAndValidateStringEnvParam(envConfigKey: keyof EnvConfig): string {
+    if (!process.env[envConfigKey]) {
+      throw new Error(
+        `${envConfigKey} environment configuration variable is not defined`,
+      );
+    }
+
+    return process.env[envConfigKey];
   }
 }
 
@@ -32,5 +55,10 @@ export interface EnvConfig {
   /**
    * App will listen this port (external API requests come to this port).
    */
-  API_SERVING_PORT: number;
+  readonly API_SERVING_PORT: number;
+  readonly MONGO_HOST: string;
+  readonly MONGO_PORT: number;
+  readonly MONGO_USER: string;
+  readonly MONGO_PASSWORD: string;
+  readonly MONGO_DB_NAME: string;
 }
