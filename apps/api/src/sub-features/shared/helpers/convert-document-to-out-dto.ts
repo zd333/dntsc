976062db73912fef607399931a791fe28950acc4
Alexander Changli <changli.alexander@gmail.com>
2019-02-01 09:35:47 +0200

    [API] get clinic tenant

--- apps/api/src/sub-features/shared/helpers/convert-document-to-out-dto.ts
+++ apps/api/src/sub-features/shared/helpers/convert-document-to-out-dto.ts
@@ -10,7 +10,7 @@ export interface DtoConstructor<T> {
 /**
  * Takes MongoDB document and converts it to output DTO.
  * Does this via class-transformer double conversion, thus DTO constructor has to be passed too.
- * Removed `_` prefixes (so that `_id` becomes `id`).
+ * Removes `_` prefixes (so that `_id` becomes `id`).
  * Excludes all properties by default, thus all properties of DTO must be decorated with `Expose`.
  */
 export function convertDocumentToOutDto<T>(params: {
--- apps/api/src/sub-features/tenants/controllers/tenants.controller.ts
+++ apps/api/src/sub-features/tenants/controllers/tenants.controller.ts
@@ -1,10 +1,22 @@
 import { ACGuard, UseRoles } from 'nest-access-control';
+import { AppRequest } from '../../../app.module';
 import { AuthGuard } from '@nestjs/passport';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { convertDocumentToOutDto } from '../../../../src/sub-features/shared/helpers/convert-document-to-out-dto';
 import { CreatedTenantOutDto } from '../dto/created-tenant.out-dto';
 import { CreateTenantInDto } from '../dto/create-tenant.in-dto';
+import { RequesterIsEmployeeOfTargetClinicGuard } from '../../shared/guards/requester-is-employee-of-target-clinic.guard';
+import { RequestIsInClinicContextGuard } from '../../shared/guards/request-is-in-clinic-context.guard';
+import { TenantDetailsOutDto } from '../dto/tenant-details.out-dto';
 import { TenantsDbConnectorService } from '../services/tenants-db-connector.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Controller('tenants')
 export class TenantsController {
@@ -25,6 +37,29 @@ export class TenantsController {
     return convertDocumentToOutDto({
       document,
       dtoConstructor: CreatedTenantOutDto,
+    });
+  }
+
+  @UseGuards(
+    AuthGuard(),
+    RequestIsInClinicContextGuard,
+    RequesterIsEmployeeOfTargetClinicGuard,
+  )
+  @Get('clinic_tenant')
+  public async getItems(@Req() req: AppRequest): Promise<TenantDetailsOutDto> {
+    const { targetClinicId } = req;
+    const document = await this.tenantsDbConnector.getByClinicId(
+      targetClinicId as string,
+    );
+
+    if (!document) {
+      // Shouldn't happen, something is definitely wrong :(
+      throw new NotFoundException();
+    }
+
+    return convertDocumentToOutDto({
+      document,
+      dtoConstructor: TenantDetailsOutDto,
     });
   }
 }
