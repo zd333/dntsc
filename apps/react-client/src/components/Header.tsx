8033789b5a89ccf0ab96e7fa573c62c0dae2ecd7
Alexander Changli <changli.alexander@gmail.com>
2019-02-06 09:52:09 +0200

    [Client] WIP header

--- apps/react-client/src/components/Header.tsx
+++ apps/react-client/src/components/Header.tsx
@@ -1,6 +1,74 @@
 import * as React from 'react';
+import { ExitToApp, Menu } from '@material-ui/icons';
+import { LEFT_NAV_WIDTH } from './Shell';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Theme,
+  createStyles,
+  WithStyles,
+  withStyles,
+  IconButton,
+} from '@material-ui/core';
 
-// TODO: implement
-export const Header: React.SFC = () => {
-  return <div>HEADER</div>;
+// TODO: add language selection support
+export interface HeaderProps {
+  readonly title: string;
+  readonly onLogout: () => void;
+  readonly onMenuClick: () => void;
+}
+
+const StyledHeader: React.SFC<StyledHeaderProps> = props => {
+  const { classes, title, onLogout, onMenuClick } = props;
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <IconButton
+        color="inherit"
+        aria-label="Toggle menu"
+        onClick={onMenuClick}
+        className={classes.menuButton}
+      >
+        <Menu />
+      </IconButton>
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          {title}
+        </Typography>
+      </Toolbar>
+      <IconButton
+        color="inherit"
+        aria-label="Logout"
+        onClick={onLogout}
+        className={classes.logoutButton}
+      >
+        <ExitToApp />
+      </IconButton>
+    </AppBar>
+  );
 };
+
+// TODO: make mobile friendly
+const headerStyles = ({ breakpoints }: Theme) =>
+  createStyles({
+    appBar: {
+      marginLeft: LEFT_NAV_WIDTH,
+      [breakpoints.up('sm')]: {
+        width: `calc(100% - ${LEFT_NAV_WIDTH}px)`,
+      },
+    },
+    logoutButton: {
+      marginRight: 20,
+    },
+    menuButton: {
+      marginRight: 20,
+      [breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+  });
+
+type StyledHeaderProps = HeaderProps & WithStyles<typeof headerStyles>;
+
+export const Header = withStyles(headerStyles)(StyledHeader);
--- apps/react-client/src/containers/HeaderContainer.ts
+++ apps/react-client/src/containers/HeaderContainer.ts
@@ -1,8 +1,31 @@
 import { connect } from 'react-redux';
-import { Header } from '../components/Header';
+import { Header, HeaderProps } from '../components/Header';
+import { RootState } from '..';
+import { selectCurrentPageName } from '../selectors/current-page-name.selector';
+import { selectCurrentUserName } from '../selectors/current-user-name.selector';
+import { SessionActions } from '../actions/session.actions';
+import {
+  DispatchMapper,
+  StateMapper,
+} from '../shared/interfaces/container-state-mapper.interface';
 
-// TODO: implement
+// TODO: replace this with ShellContainer
+
+const mapStateToProps: StateMapper<HeaderProps, RootState> = state => {
+  return {
+    title: `${selectCurrentPageName(state)} (${selectCurrentUserName(state)})`,
+  };
+};
+
+const mapDispatchToProps: DispatchMapper<HeaderProps> = dispatch => {
+  return {
+    onLogout: () => {
+      dispatch(SessionActions.logout());
+    },
+  };
+};
+
 export const HeaderContainer = connect(
-  () => void 0,
-  () => void 0,
+  mapStateToProps,
+  mapDispatchToProps,
 )(Header);
