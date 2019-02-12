88c476f3d57e86c6e8b4a8d7344c9bd62eb3ad2f
Alexander Changli <changli.alexander@gmail.com>
2019-02-12 10:39:30 +0200

    [Client] shell fader and spinner

--- apps/react-client/src/components/Shell.tsx
+++ apps/react-client/src/components/Shell.tsx
@@ -1,17 +1,32 @@
 import * as React from 'react';
 import { AppLanguages } from '../reducers/session-state.interface';
-import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core';
 import { Header, HeaderProps } from './Header';
 import { MainMenu, MainMenuProps } from './MainMenu';
+import {
+  createStyles,
+  Theme,
+  WithStyles,
+  withStyles,
+  CircularProgress,
+  Backdrop,
+} from '@material-ui/core';
 
 // This value is used in main menu (drawer) and in header (app bar)
 export const MAIN_MENU_WIDTH = 300;
+// Default value from material ui lib
+const SPINNER_WIDTH = 40;
+const BUSY_ANIMATION_TIMEOUT = 1000;
+
 const languageOptions: Array<AppLanguages> = Object.keys(AppLanguages).map(
   key => AppLanguages[key],
 );
 
 export type ShellProps = {
+  /**
+   * Need route path just to toggle main menu on mobile when it changes.
+   */
   readonly routePath: string;
+  readonly isBusy: boolean;
 } & Pick<
   HeaderProps,
   Exclude<keyof HeaderProps, 'languageOptions' | 'onMenuClick'>
@@ -47,6 +62,7 @@ export class StyledShell extends React.Component<StyledShellProps, ShellState> {
 
   public render(): JSX.Element {
     const {
+      isBusy,
       classes,
       title,
       currentLanguage,
@@ -76,6 +92,18 @@ export class StyledShell extends React.Component<StyledShellProps, ShellState> {
         {/* Main content (page component) */}
         <main className={classes.content}>
           <div className={classes.toolbar} />
+
+          <Backdrop
+            open={isBusy}
+            classes={{ root: classes.backdropOverlay }}
+            transitionDuration={{
+              enter: BUSY_ANIMATION_TIMEOUT,
+              exit: 0,
+            }}
+          />
+
+          {isBusy && <CircularProgress className={classes.spinner} />}
+
           {children}
         </main>
       </div>
@@ -83,7 +111,7 @@ export class StyledShell extends React.Component<StyledShellProps, ShellState> {
   }
 }
 
-const shellStyles = ({ spacing, mixins }: Theme) =>
+const shellStyles = ({ spacing, mixins, palette }: Theme) =>
   createStyles({
     root: {
       display: 'flex',
@@ -94,7 +122,19 @@ const shellStyles = ({ spacing, mixins }: Theme) =>
     },
     content: {
       flexGrow: 1,
+      minHeight: '100vh',
+      position: 'relative',
       padding: spacing.unit * 3,
+    },
+    spinner: {
+      position: 'absolute',
+      top: `calc(50vh - ${SPINNER_WIDTH / 2}px)`,
+      left: `calc(50% - ${SPINNER_WIDTH / 2}px)`,
+    },
+    backdropOverlay: {
+      zIndex: 1099,
+      // Do not use palette because there is no suitable grey with opacity and this is single usage of this color
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
   });
 
