ff95bc998eb9978539596806064e78ac8642696f
Alexander Changli <changli.alexander@gmail.com>
2019-02-28 09:44:28 +0200

    [Clisent] WIP update inventory item connector and epic

--- apps/react-client/src/sub-features/inventory/api-connectors/index.ts
+++ apps/react-client/src/sub-features/inventory/api-connectors/index.ts
@@ -1,5 +1,6 @@
 import { createInventoryItemApiConnector } from './create-inventory-item.api-connector';
 import { searchInventoryItemsApiConnector } from './search-inventory-items.api-connector';
+import { updateInventoryItemApiConnector } from './update-inventory-item.api-connector';
 
 /**
  * This should have structure that follows epic middleware dependency injection interface.
@@ -8,4 +9,5 @@ import { searchInventoryItemsApiConnector } from './search-inventory-items.api-c
 export const inventoryApiConnectors = {
   searchInventoryItemsApiConnector,
   createInventoryItemApiConnector,
+  updateInventoryItemApiConnector,
 };
